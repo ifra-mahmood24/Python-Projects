7001b977d1e33172706289d973d7c365e6a9828b
Anastasiia Sydorchuk <as4060@hw.ac.uk>
2025-11-08 22:04:03 +0000

    initial data loading

--- IP-CW2/DocTracker.py
+++ IP-CW2/DocTracker.py
@@ -5,8 +5,8 @@ class DocTracker:
         self.df = None
 
     def main(self):
-        #json_url = "http://www.macs.hw.ac.uk/~hwloidl/Courses/F21SC/issuu_cw2.json"
-        json_url = "http://www.macs.hw.ac.uk/~hwloidl/Courses/F21SC/issuu_sample.json"
+        json_url = "http://www.macs.hw.ac.uk/~hwloidl/Courses/F21SC/issuu_cw2.json"
+        #json_url = "http://www.macs.hw.ac.uk/~hwloidl/Courses/F21SC/issuu_sample.json"
 
         loader = DataExtractor(json_url)
         df = loader.load()
