4de73c52eb173a3436179c3e67429eda9668812f
Anastasiia Sydorchuk <as4060@hw.ac.uk>
2025-11-08 21:55:40 +0000

    initial data loading

--- IP-CW2/DataExtractor.py
+++ IP-CW2/DataExtractor.py
@@ -1,12 +1,47 @@
+import json
+import httpx
+import pandas
+
 class DataExtractor:
     #all document related functionalities
-    def __init__(self):
-        pass
+    def __init__(self, source):
+        self.source = source
+        self.df = None
+        self.data = None
+
+    def load(self):
+        text = self.loadFromUrl()
+        self.data = self.parseJSON(text)
+        self.df = pandas.DataFrame(self.data)
+        return self.df
+        
+    def parseJSON(self, text):
+        text = text.strip()
+        if not text:
+            return []
+        try:
+            lines = text.splitlines()
+            parsedLines = [json.loads(line) for line in lines if line.strip()]
+            return parsedLines
+        except Exception:
+            print("Failed to parse")
+            return []
+            
+    def loadFromUrl(self):
+        try:
+            response = httpx.get(self.source)
+            response.raise_for_status()
+            return response.text
+        except httpx.RequestError:
+            print("Failed to load - RequestError")
+            return ""
+        except httpx.HTTPStatusError:
+            print("Failed to load - HTTPStatusError")
+            return ""
 
     def verifyExtension(doc):
         pass
 
     def getContent(doc):
         pass
-
     
--- IP-CW2/DocTracker.py
+++ IP-CW2/DocTracker.py
@@ -1,9 +1,19 @@
+from DataExtractor import DataExtractor
+
 class DocTracker:
     def __init__(self):
-        pass
+        self.df = None
+
+    def main(self):
+        #json_url = "http://www.macs.hw.ac.uk/~hwloidl/Courses/F21SC/issuu_cw2.json"
+        json_url = "http://www.macs.hw.ac.uk/~hwloidl/Courses/F21SC/issuu_sample.json"
 
 
-    def main():
-        pass
+        loader = DataExtractor(json_url)
+        df = loader.load()
+        print(df)
 
-    
+if __name__ == "__main__":
+    app = DocTracker()
+    app.main()
+
