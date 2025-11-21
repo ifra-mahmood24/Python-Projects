2ba7b3d6b54ea22f22a5f12a2d7632d564aff4ef
Anastasiia Sydorchuk <as4060@hw.ac.uk>
2025-11-21 23:05:09 +0000

    added avid readers functionality

--- IP-CW2/AvidReaders.py
+++ IP-CW2/AvidReaders.py
@@ -1,11 +1,14 @@
 class AvidReaders:
-    def __init__(self):
-        pass
+    def __init__(self, df_copy):
+        self.df_copy = df_copy
 
-
-    def getAvidReaders(docID):
-        pass
-        #returns list of avid readers
+    #returns list of avid readers
+    def getAvidReaders(self):
+        reads = self.df_copy[self.df_copy["event_type"] == "pagereadtime"] #pageread.
+        # replaces missing values with 0
+        reads["event_readtime"] = reads["event_readtime"].fillna(0)
+        total_time = reads.groupby("visitor_uuid")["event_readtime"].sum()
+        return total_time.sort_values(ascending=False).head(10)
 
     def getTableOfReaders(lstAvidReaders):
         pass
