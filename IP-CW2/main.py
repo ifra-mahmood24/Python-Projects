fcc4d4c492d03814aae00f1073075683b3217265
Anastasiia Sydorchuk <as4060@hw.ac.uk>
2025-11-25 22:57:58 +0000

    cleaned the structure

--- IP-CW2/main.py
+++ IP-CW2/main.py
@@ -1,13 +1,4 @@
-from CW2.core import run_gui
-import sys
-
+from GUI import GUI
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        # CLI mode
-        # Example:
-        #   myapp.exe -u ... -d ... -t ...
-        from CW2.CLI import main as cli_main
-        cli_main()
-    else:
-        run_gui()
+    GUI()
